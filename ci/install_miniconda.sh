c16a3021bf0374b283d3574da51c9be2296edafc
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-02-14 09:31:04 -0600

    Install miniconda

---
+++ ci/install_miniconda.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo
+echo "[install_travis]"
+
+home_dir=$(pwd)
+echo
+echo "[home_dir]: $home_dir"
+
+# install miniconda
+MINICONDA_DIR="$HOME/miniconda3"
+
+time wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -q -O miniconda.sh || exit 1
+time bash miniconda.sh -b -p "$MINICONDA_DIR" || exit 1
+
+echo
+echo "[show conda]"
+which conda
+
+echo
+echo "[update conda]"
+conda config --set quiet true --set always_yes true --set changeps1 false || exit 1
+conda update -q conda
+
+conda install -yq conda-build 
