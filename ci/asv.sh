373773fecf98287f048f88d9ed6c16ef8950e1e4
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-02-14 09:19:56 -0600

    Setup CI

---
+++ ci/asv.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "inside $0"
+
+source activate pandas
+
+RET=0
+
+echo "[ASV start]"
+
+cd asv_bench
+
+asv machine --yes
+
+time asv dev | tee failed_asv.txt
+
+echo "The following asvs benchmarks (if any) failed."
+
+cat failed_asv.txt | grep "failed" failed_asv.txt
+
+if [ $? = "0" ]; then
+    RET=1
+fi
+
+echo "DONE displaying failed asvs benchmarks."
+
+rm failed_asv.txt
+
+echo "[ASV DONE]"
