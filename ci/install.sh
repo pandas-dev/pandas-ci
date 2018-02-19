61ad34bc2e20863dfe8c658362d286fd3f01fbad
Jeff Reback <jeff@reback.net>
2018-02-19 15:09:50 -0500

    fix pip

--- ci/install.sh
+++ ci/install.sh
@@ -7,22 +7,22 @@ source activate pandas
 if [ "$JOB" == "ASV" ]; then
 
     echo "[building asv]"
-    conda build pandas/conda.recipe/meta.yaml --python=3
-    conda install -n pandas $(conda build --output pandas/conda.recipe/meta.yaml --python=3)
+    time conda build pandas/conda.recipe/meta.yaml --python=3
+    time conda install -n pandas $(conda build --output pandas/conda.recipe/meta.yaml --python=3)
 
 elif [ "$JOB" == "PIP" ]; then
 
     echo "[building release]"
-    time bash pandas/scripts/build_dist_for_release.sh || exit 1
-    conda uninstall -y cython
+    push pandas
+    time bash scripts/build_dist_for_release.sh || exit 1
+    time conda uninstall -y cython
     time pip install dist/*tar.gz || exit 1
+    popd
 
 elif [ "$JOB" == "CONDA" ]; then
 
     echo "[conda]"
-    conda install conda-build
-
-    conda build pandas/conda.recipe/meta.yaml --python=3
-    conda install -n pandas $(conda build --output pandas/conda.recipe/meta.yaml --python=3)
+    time conda build pandas/conda.recipe/meta.yaml --python=3
+    time conda install -n pandas $(conda build --output pandas/conda.recipe/meta.yaml --python=3)
 
 fi
