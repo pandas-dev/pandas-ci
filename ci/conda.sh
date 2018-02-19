70a839e86d06c462b0d8bca7f6aada92012cea04
Jeff Reback <jeff@reback.net>
2018-02-19 15:17:41 -0500

    small corrections

--- ci/conda.sh
+++ ci/conda.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
 
 echo "inside $0"
 
 source activate pandas
-
-RET=0
 
 echo "[conda-test]"
 
@@ -12,7 +10,4 @@ echo "[running]"
 cd /tmp
 unset PYTHONPATH
 
-echo "[build-test: not single]"
 python -c 'import pandas; pandas.test(["-n 2", "--skip-slow", "--skip-network", "-r xX", "-m not single"])'
-
-exit "$RET"
