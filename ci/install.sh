bbd0b129358a692e7b718b150afc52e5f17bd7fe
Jeff Reback <jeff@reback.net>
2018-07-30 07:40:56 -0400

    add more verbose

--- ci/install.sh
+++ ci/install.sh
@@ -2,27 +2,37 @@ echo "inside $0"
 
 echo "Running ${JOB}"
 
-source activate pandas
-
 if [ "$JOB" == "ASV" ]; then
 
     echo "[building asv]"
     time conda build pandas/conda.recipe/meta.yaml --python=3.6 --numpy=1.14
+
+    echo "[installing asv]"
     time conda install -n pandas $(conda build --output pandas/conda.recipe/meta.yaml --python=3.6 --numpy=1.14)
 
 elif [ "$JOB" == "PIP" ]; then
 
-    echo "[building release]"
+    source activate pandas
+
     pushd pandas
+
+    echo "[building dist]"
     time bash scripts/build_dist_for_release.sh || exit 1
+
+    echo "[uninstall cython]"
     time conda uninstall -y cython
+
+    echo "[pip install]"
     time pip install dist/*tar.gz || exit 1
+
     popd
 
 elif [ "$JOB" == "CONDA" ]; then
 
-    echo "[conda]"
+    echo "[build conda]"
     time conda build pandas/conda.recipe/meta.yaml --python=3.6 --numpy=1.14
+
+    echo "[install conda]"
     time conda install -n pandas $(conda build --output pandas/conda.recipe/meta.yaml --python=3.6 --numpy=1.14)
 
 fi
