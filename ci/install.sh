0d09fa6607769c46872a60aafddbb03405359600
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-03-06 08:24:57 -0600

    CI: Update for pandas-25193

--- ci/install.sh
+++ ci/install.sh
@@ -5,7 +5,7 @@ echo "Running ${JOB}"
 if [ "$JOB" == "ASV" ]; then
 
     echo "[building asv]"
-    time conda build pandas/conda.recipe/meta.yaml --python=3.6 --numpy=1.14
+    time conda build pandas/conda.recipe/meta.yaml --python=3.7 --numpy=1.14
 
     echo "[installing asv]"
     time conda install -n pandas --use-local pandas
@@ -19,8 +19,11 @@ elif [ "$JOB" == "PIP" ]; then
     echo "[building dist]"
     time bash scripts/build_dist_for_release.sh || exit 1
 
-    echo "[uninstall cython]"
-    time conda uninstall -y cython
+    echo "[create fresh environment]":
+    conda create -n pip-test-env python=3.7 -yq
+    source activate pip-test-env
+    echo "[pip-test-env before]"
+    conda list
 
     echo "[pip install]"
     time pip install dist/*tar.gz || exit 1
@@ -30,7 +33,7 @@ elif [ "$JOB" == "PIP" ]; then
 elif [ "$JOB" == "CONDA" ]; then
 
     echo "[build conda]"
-    time conda build pandas/conda.recipe/meta.yaml --python=3.6 --numpy=1.14
+    time conda build pandas/conda.recipe/meta.yaml --python=3.7 --numpy=1.14
 
     echo "[install conda]"
     time conda install -n pandas --use-local pandas
